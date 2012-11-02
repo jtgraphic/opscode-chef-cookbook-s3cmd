e9f9c6380b46aae3187465883d38b517d0358fff
James Thompson <me@jtgraphic.net>
2012-11-02 16:30:33 -0400

    adding attribute and user support for the configuration file

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+# Users receiving configuration
+default[:s3cmd][:users] = []
+
+# Credentials
+default[:s3cmd][:aws_access_key_id] = ""
+default[:s3cmd][:aws_secret_access_key] = ""
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,17 @@
 # Cookbook Name:: s3cmd
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, jtgraphic
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node[:s3cmd][:users].each do |user|   
+    home = user.to_s == :root.to_s ? "/root" : "/home/#{user}"
+
+    template "s3cfg" do
+        path "#{home}/.s3cfg"
+        source "s3cfg.erb"
+        mode 0655
+    end
+end
